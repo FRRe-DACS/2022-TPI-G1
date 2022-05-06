5f37b0e3e2be4e146411099f4c4701cf8ce87f69
unknown <xlmriosx@gmail.com>
2022-05-06 18:30:31 -0300

    added connection database mysql and tests templates to login

---
+++ src/test/java/dacs/tpi/g1/G1ApplicationTests.java
@@ -0,0 +1,13 @@
+package dacs.tpi.g1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class G1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
